f8b60409e658d5236a5eb281343370ca8075cd1b
Kamil Chmiel <kamil.chmiel@icloud.com>
2022-12-19 12:25:08 +0100

    KOD-19.2

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,46 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
+    }
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
+        return new TaskDto(taskId, "test title", "test_content");
+    }
+
+    @DeleteMapping(value = "{taskId}")
+    public void deleteTask(@PathVariable Long taskId) {
+
+    }
+
+    @PutMapping
+    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
+        return new TaskDto(1L, "Edited test title", "Test content");
+    }
+
+    @PostMapping
+    public void createTask(@RequestBody TaskDto taskDto) {
+        System.out.println("Otrzymalem body " + taskDto);
+    }
+}
